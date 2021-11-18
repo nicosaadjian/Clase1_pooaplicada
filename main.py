86c486979aaef31dc8b2739d3e08b8b0ad41eb6a
nicosaadjian <70555858+nicosaadjian@users.noreply.github.com>
2021-11-18 20:19:01 -0300

    Se actualiza el main.py
    Se agrego una funcion para que el usuario pueda manipular el TAD cola, implementandolo pensado como para una fila de supermercado, siendo el usuario el cajero.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Fila:
         
     def agregarPersona(self, persona):
         self.fila.append(persona)
-        print(self.fila)
+        print("Personas por atender: ", self.fila)
         
     def personaAtendida(self):
         filaInvertida = self.fila[::-1]
@@ -24,3 +24,23 @@ fila.agregarPersona("Carlos")
 fila.personaAtendida()
 fila.personaAtendida()
 fila.personaAtendida()
+
+#Esta funcion servira para que el usuario pueda manipular la fila mediante una GUI
+
+def funcionUsuario():
+    fila = Fila()
+    print("Bienvenido. Puede ingresar un numero y se agregara una persna o atenderá a la primer persona de la fila")
+    print("1. Ingresar persona")
+    print("2. Atender persona")
+    print("3. Cerrar")
+    opcion = int(input("Ingrese una opcion de la lista"))
+    if(opcion == 1):
+        nombrePersona = input("Ingrese el nombre del cliente")
+        fila.agregarPersona(nombrePersona)
+    elif(opcion == 2):
+        fila.personaAtendida()
+    elif(opcion == 3):
+        print("Gracias por usar el sistema!")
+        
+funcionUsuario()
+    
